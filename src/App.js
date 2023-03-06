1b3da0fba6937e552aff2aaf936212c326f53457
able c j <gracyebin7@gamil.com>
2023-03-07 00:50:05 +0530

    Netflix project post added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import React from 'react';
+import {orginals,action} from './urls'
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RawPost/RowPost';
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost url={orginals} title='Netflix Originals' />
+      <RowPost url={action} title='Action' isSmall />
     </div>
   );
 }
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "4defeff328ccb4d24c44297bf3c5d7ff";
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
